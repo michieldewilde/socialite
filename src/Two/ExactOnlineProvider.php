df371fe3f8a57f0a927731ab85213827b33de1a9
michieldewilde <mich.dewild@gmail.com>
2016-09-03 16:39:26 +0200

    Add exactonline

---
+++ src/Two/ExactOnlineProvider.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\CloudServices\OAuth2;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use GuzzleHttp\Psr7;
+
+class ExactOnlineProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'EXACTONLINE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [''];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getInstanceUri().'oauth2/auth', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getInstanceUri().'oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getInstanceUri().'v1/current/Me?$select=UserName', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+
+        // parse response (has to be extracted into function)
+        Psr7\rewind_body($response);
+        $json = json_decode($response->getBody()->getContents(), true);
+        if (array_key_exists('d', $json)) {
+            if (array_key_exists('results', $json['d'])) {
+                if (count($json['d']['results']) == 1) {
+                    return $json['d']['results'][0];
+                }
+                if (array_key_exists('__next', $json['d'])) {
+                    $this->nextUrl = $json['d']['__next'];
+                }
+                else {
+                    $this->nextUrl = null;
+                }
+                return $json['d']['results'];
+            }
+            return $json['d'];
+        }
+
+        return $json;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'name' => $user['UserName'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getInstanceUri()
+    {
+        return $this->getConfig('instance_uri', 'https://start.exactonline.nl/api/');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['instance_uri'];
+    }
+
+}
